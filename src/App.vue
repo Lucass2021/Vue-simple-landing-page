2fa13f734c9115772e8f8c9b42301651c44ca64a
Lucas Dias <lucas.allx@hotmail.com>
2024-01-08 19:07:29 -0300

    menu initial

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,22 @@
 <template>
   <div>
-    <h1>Hello world</h1>
+    <MenuComponent/>
   </div>
 </template>
 
 <script setup>
+import MenuComponent from './components/MenuComponent.vue';
+
 
 
 </script>
 
 <style>
+*{
+  margin:0;
+  padding:0;
+  box-sizing: border-box;
+  font-family: 'Open Sans', sans-serif;
+}
 
 </style>
---
+++ src/components/MenuComponent.vue
@@ -0,0 +1,40 @@
+<template>
+    <div class="menu-container">
+        <ul class="menu">
+            <li><strong>Data</strong>Sphere</li>
+            <li>Apply for access</li>
+        </ul>
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+
+.menu-container{
+    margin: 0 100px;
+    margin-top:30px;
+}
+
+.menu{
+    list-style: none;
+    display: flex;
+    justify-content: space-between;
+    padding:0;
+}
+
+.menu li:first-child{
+    
+}
+.menu li:last-child{
+    cursor:pointer;
+    font-weight: 500;
+}
+
+.menu li:last-child:hover{
+    border-bottom: 3px solid #43FFA1;
+}
+
+</style>
