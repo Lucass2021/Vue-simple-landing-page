0dc21c6e1bf16de82cf781014791da3e6223ab99
Lucas Dias <lucas.allx@hotmail.com>
2024-01-08 20:12:12 -0300

    banner new image initial

--- src/components/BannerComponent.vue
+++ src/components/BannerComponent.vue
@@ -3,10 +3,14 @@
         <div>
             <slot></slot>
         </div>
-
+        
         <div class="title-container">
             <h1>Data <span>tailored</span> to your needs.</h1>
             <button>Learn More</button>
+        </div>
+
+        <div class="image-container">
+            <img src="../assets/phone.png" alt="">
         </div>
     </div>
 </template>
@@ -18,7 +22,7 @@
 <style scoped>
 
 .banner-container{
-    background-image: url("../assets/banner-bg.svg");
+    background-image: url("../assets/bg.png");
     background-position: center;
     background-size: cover;
     height: 100vh;
@@ -41,6 +45,18 @@ h1{
 
 h1 span{
     border-bottom: 3px solid #43FFA1;
+}
+
+.image-container{
+    display: flex;
+    justify-content: center;
+}
+
+img{
+    position: relative;
+    top:50px;
+    width:400px;
+    transform: rotate(10deg);
 }
 
 </style>
